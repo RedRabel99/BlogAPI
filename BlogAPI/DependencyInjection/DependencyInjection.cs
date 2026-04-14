4bbe9fb99217f70e4605bfd012a16b1a5478816a
Paweł Lysko <lyskopawel03@gmail.com>
2026-04-14 22:34:55 +0200

    remove unused code

--- BlogAPI/DependencyInjection/DependencyInjection.cs
+++ BlogAPI/DependencyInjection/DependencyInjection.cs
@@ -59,43 +59,6 @@ public static class DependencyInjection
                 [new OpenApiSecuritySchemeReference("bearer", document)] = []
             });
         });
-        //services.AddSwaggerGen(options =>
-        //{
-        //    options.SwaggerDoc("v1", new OpenApiInfo()
-        //    {
-        //        Title = "Blog Api",
-        //        Version = "v1"
-        //    });
-        //    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-        //    {
-        //        In = ParameterLocation.Header,
-        //        Description = "Enter a bearer token",
-        //        Name = "Authorization",
-        //        Type = SecuritySchemeType.Http,
-        //        BearerFormat = "JWT",
-        //        Scheme = "bearer",
-        //        Flows= new OpenApiOAuthFlow
-        //        {
-        //            Authoriza
-        //        }
-        //    });
-
-        //options.AddSecurityRequirement(new OpenApiSecurityRequirement
-        //{
-        //    {
-        //        new OpenApiSecurityScheme
-        //        {
-        //            Reference = new OpenApiReference
-        //            {
-        //                Type = ReferenceType.SecurityScheme,
-        //                Id = "Bearer"
-        //            }
-        //        },
-        //        []
-        //    }
-        //}
-        //);
-    //});
 
         services.AddExceptionHandler<GlobalExceptionHandler>();
 
