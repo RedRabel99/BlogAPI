e43ced3299ab2f7fc10b29bce13ec4f0ba2c39e3
Pawel Lysko <lyskopawel03@gmail.com>
2026-04-18 15:56:11 +0200

    Create integration tests project

---
+++ BlogAPI.IntegrationTests/Infrastructure/IntegrationTestFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
+
+namespace BlogAPI.IntegrationTests.Infrastructure;
+
+public class IntegrationTestFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbcContainer;
+
+    public IntegrationTestFactory()
+    {
+        _dbcContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .WithDatabase("blogapi-db")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+    }
+
+    public async Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
