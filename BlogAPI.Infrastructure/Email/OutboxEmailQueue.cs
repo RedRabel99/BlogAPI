af02f9f346b0a0d41ec488faf36371b28c46f090
Paweł Lysko <lyskopawel03@gmail.com>
2026-05-29 01:52:10 +0200

    remove unused email sender

--- BlogAPI.Infrastructure/Email/OutboxEmailQueue.cs
+++ BlogAPI.Infrastructure/Email/OutboxEmailQueue.cs
@@ -7,12 +7,11 @@ namespace BlogAPI.Infrastructure.Email;
 public sealed class OutboxEmailQueue : IEmailQueue
 {
     private readonly AppDbContext _context;
-    private readonly IEmailSender _emailSender;
+    
 
     public OutboxEmailQueue(AppDbContext context, IEmailSender emailSender)
     {
         _context = context;
-        _emailSender = emailSender;
     }
 
     public async Task EnqueueToOutbox(EmailMessage message, CancellationToken ct = default)
