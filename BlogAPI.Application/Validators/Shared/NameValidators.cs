81f0762eab32d3ef14b1eee106efa55779888b1f
Paweł Lysko <lyskopawel03@gmail.com>
2026-06-01 01:43:00 +0200

    Fix a bug allowing username to start with dash or underscore

--- BlogAPI.Application/Validators/Shared/NameValidators.cs
+++ BlogAPI.Application/Validators/Shared/NameValidators.cs
@@ -16,6 +16,6 @@ public static class NameValidators
         return ruleBuilder
             .CommonNameRules()
             .Matches("^[a-zA-Z0-9-_]+$").WithMessage("Only letters, numbers, -, _ are allowed")
-            .Must(x => !x.StartsWith('-') || !x.StartsWith('_')).WithMessage("Cannot start with - or _ characters");
+            .Must(x => !x.StartsWith('-') && !x.StartsWith('_')).WithMessage("Cannot start with - or _ characters");
     }
 }
